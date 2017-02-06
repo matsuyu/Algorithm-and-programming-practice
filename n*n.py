5ca84591854ad254d18e8800da61a528c6d57ab3
matsuyu <wang.yuheng@dhs.sg>
2017-02-06 08:57:33 +0800

    Update n*n.py

--- n*n.py
+++ n*n.py
@@ -1,1 +1,11 @@
 
+havePair = False
+a=[1,2,4,4]
+sum = 8
+for i in range(1,len(a)):
+    for j in range (i):
+        if a[i]+a[j]== sum :
+            print('Y',"Positional index:",j+1,i+1,"Number:",a[i],a[j])
+            havePair = True
+if not havePair:
+    print('N')
